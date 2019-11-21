0572af75bcc8c146028eb7104151580fdebeed43
amine <labassiamine@gmail.com>
2019-11-21 22:48:36 +0100

    restore msg cle

--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { LayerStackComponent } from './layer-stack/layer-stack.component';
 import { HeaderComponent } from './header/header.component';
-import { LayerComponent } from './layer/layer.component';
-import { ImgBgComponent } from './img-bg/img-bg.component';
-import { ContentComponent } from './content/content.component';
 import { PrevnextComponent } from './prevnext/prevnext.component';
 import { ConfirmPopinComponent } from './confirm-popin/confirm-popin.component';
-import { TabBtnComponent } from './tab-btn/tab-btn.component';
-import { TabBtnGrpComponent } from './tab-btn-grp/tab-btn-grp.component';
 import { FormsModule } from '@angular/forms';
 import { WideImgBgComponent } from './wide-img-bg/wide-img-bg.component';
 import { SceneDirective } from './directives/scene.directive';
 import { SceneElementDirective } from './directives/scene-element.directive';
 import { HeaderDirective } from './directives/header.directive';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { CardMsgCleComponent } from './card-msg-cle/card-msg-cle.component';
 import { WallpaperDirective } from './directives/wallpaper.directive';
 import { ProgressComponent } from './progress/progress.component';
 import { DimSyncDirective } from './directives/dim-sync.directive';
@@ -24,15 +19,9 @@ import { InsertComponent } from './insert/insert.component';
 
 @NgModule({
   declarations: [
-    LayerStackComponent,
     HeaderComponent,
-    LayerComponent,
-    ImgBgComponent,
-    ContentComponent,
     PrevnextComponent,
     ConfirmPopinComponent,
-    TabBtnComponent,
-    TabBtnGrpComponent,
     WideImgBgComponent,
     SceneDirective,
     SceneElementDirective,
@@ -41,7 +30,8 @@ import { InsertComponent } from './insert/insert.component';
     WallpaperDirective,
     ProgressComponent,
     DimSyncDirective,
-    InsertComponent
+    InsertComponent,
+    CardMsgCleComponent,
   ],
   imports: [
     CommonModule,
@@ -50,14 +40,8 @@ import { InsertComponent } from './insert/insert.component';
   ],
   exports: [
     HeaderComponent,
-    LayerStackComponent,
-    LayerComponent,
-    ImgBgComponent,
-    ContentComponent,
     PrevnextComponent,
     ConfirmPopinComponent,
-    TabBtnComponent,
-    TabBtnGrpComponent,
     WideImgBgComponent,
     SceneElementDirective,
     SceneDirective,
@@ -66,7 +50,8 @@ import { InsertComponent } from './insert/insert.component';
     WallpaperDirective,
     ProgressComponent,
     DimSyncDirective,
-    InsertComponent
+    InsertComponent,
+    CardMsgCleComponent,
   ]
 })
 export class ThemeModule { }
